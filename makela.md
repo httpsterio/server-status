f22ae6ee9f88747d864c62d96d3b4c734aa43358
makela <makela@sami@httpster.io>
2022-01-06 05:35:01 +0200

    update

---
+++ makela.md
@@ -0,0 +1,8 @@
+2022-01-06 05:16:14
+OK, 2 working disks
+2022-01-06 05:24:01
+OK, 2 working disks
+2022-01-06 05:26:01
+OK, 2 working disks
+2022-01-06 05:30:01
+OK, 2 working disks
---
+++ raid-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+WORKING=$(mdadm -D /dev/md0 | grep "Working" | grep -Eo '[0-9]')
+FAILED=$(mdadm -D /dev/md0 | grep "Failed" | grep -Eo '[0-9]')
+
+echo $(date +"%F %T")
+
+if (($WORKING != 2)); 
+then
+  echo "ERROR," $FAILED "failed disks"
+else
+  echo "OK," $WORKING "working disks"
+fi
