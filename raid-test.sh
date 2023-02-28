d432e1634537ca806a6cb6f078f3ab4cfd261ea6
Sami Mäkelä <sami@httpster.io>
2023-03-01 01:52:18 +0200

    update

--- raid-test.sh
+++ raid-test.sh
@@ -6,7 +6,7 @@ FAILED=$(mdadm -D /dev/md0 | grep "Failed" | grep -Eo '[0-9]')
 
 if (($WORKING != 2));
 then
-  echo "|" $(date +'%Y-%m-%d') "|" $(date +"%H:%M") "|" "ERROR", $FAILED "failed disks |"  >> README.md
+  echo "|" $(date +'%Y-%m-%d') "|" $(date +"%H:%M") "|" "ERROR", $FAILED "failed disks |"  >> /home/makela/scripts/server-status/README.md
 else
-  echo "|" $(date +'%Y-%m-%d') "|" $(date +"%H:%M") "|" "OK", $WORKING "working disks |"  >> README.md
+  echo "|" $(date +'%Y-%m-%d') "|" $(date +"%H:%M") "|" "OK", $WORKING "working disks |"  >> /home/makela/scripts/server-status/README.md
 fi
