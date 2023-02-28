5a66df3dbf3822a89d6e6e2c73561d131bcf501c
Sami Mäkelä <sami@httpster.io>
2023-02-28 21:27:52 +0000

    checking disks

--- raid-test.sh
+++ raid-test.sh
@@ -3,13 +3,14 @@
 WORKING=$(mdadm -D /dev/md0 | grep "Working" | grep -Eo '[0-9]')
 FAILED=$(mdadm -D /dev/md0 | grep "Failed" | grep -Eo '[0-9]')
 
-echo $(date +"%F %T")
+#echo $(date +"%F %T") >> makela.md
 
-if (($WORKING != 2)); 
+#echo "|" $(date +'%Y-%m-%d') "|" $(date +"%H:%M") "|" >> makela.md
+
+if (($WORKING != 2));
 then
-  echo "ERROR," $FAILED "failed disks"
-  echo ""
+  echo "|" $(date +'%Y-%m-%d') "|" $(date +"%H:%M") "|" "ERROR", $FAILED "failed disks |"  >> makela.md
+# echo "ERROR," $FAILED "failed disks" >> makela.md
 else
-  echo "OK," $WORKING "working disks"
-  echo ""
+  echo "|" $(date +'%Y-%m-%d') "|" $(date +"%H:%M") "|" "OK", $WORKING "working disks |"  >> makela.md
 fi
